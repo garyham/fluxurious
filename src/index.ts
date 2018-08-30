84c20876497426ed91f6eeb39f2e71725b0fefff
Gary Hamilton <g.hamilton.home@gmail.com>
2018-08-30 11:04:21 +0100

    Adding logger and fixing package.json

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Store, StoreLogger } from './store';
 export { Event, EventLogger } from './event';
 export { DispatcherWrapperFn, ReducerFn, SubscriberFn, UnsubFn } from './event';
 export { Context } from './context';
-export { standardEventLogger, standardStoreLogger } from './log';
+export { standardEventLogger, standardStoreLogger, standardContextLogger } from './log';
